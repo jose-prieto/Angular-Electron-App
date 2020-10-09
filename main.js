f249de06c12cdcfe2580ba0b2397c1f1938e395f
jose-prieto <jap25o94@gmail.com>
2020-10-08 23:22:05 -0400

    commit definitivo

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Crea la ventana del navegador.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // y carga el index.html de la aplicación.
+  win.loadFile('dist/angu-elect/index.html')
+}
+
+app.whenReady().then(createWindow)
